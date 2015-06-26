d6425ea0e4e5954452af2415eef9933181541b3e
imexpeditor <imexpeditor@gmail.com>
2015-06-26 17:15:04 +0300

    diger menusune troll secenegi

--- README.md
+++ README.md
@@ -1,31 +1,31 @@
 # Ekşi Reaktör
-Ekşi Reaktör, [ekşisözlük](https://eksisozluk.com) deneyimini iyileştirmek için faydalı bir Chrome uzantısıdır.
+[ekşisözlük](https://eksisozluk.com) deneyimini iyileştirmek için faydalı bir Chrome uzantısı.
 
 ![Ekşi Reaktör ekran görüntüsü](https://i.imgur.com/fHVIzF8.png)
 
-  - Sol frame'de seçtiğiniz kelikemeleri gözlerden ırak tutabilir.
+  - Sol frame'de seçtiğiniz kelimeleri gözlerden ırak tutabilir.
   - Entrylerin arasına karışan reklam başlığını engeller.
   - Dikkat dağıtmasını engellemek için sol frame'i gizleyebilir.
   - Konulu videoları da gizleyebilir.
   - Hatta konulu videolarda 59saniye yerine Youtube da kullanabilir.
-  - *Üşengeçler için troll'lere eklemeyi kolayştırır. (yakında)*
+  - Üşengeçler için troll'lere eklemeyi kolaylaştırır.
 
 ### Kelime filtresi
-Sol frame için kelime sansürünü açayım: [jiddu krişnamurti](https://eksisozluk.com/jiddu-krisnamurti--94693) başlığını ele alalım. Mesela `nam` kelimesini sansürlediğimiz takdirde `jiddu krişnamurti` içinde `nam` olduğundan dolayı sol frame'den uçurulacaktır. Belki ilerleyen zamanlarda, talep olursa daha gelişmiş filtreler eklenebilir.
+Sol frame için kelime sansürünü açayım: [jiddu krişnamurti](https://eksisozluk.com/jiddu-krisnamurti--94693) başlığını ele alalım. Mesela `nam` kelimesini sansürlediğimiz takdirde `jiddu krişnamurti` içinde `nam` olduğundan dolayı sol frame'den uçurulacaktır. Belki ilerleyen zamanlarda, talep olursa daha gelişmiş filtreler eklenebilir. `meltem banko` gibi birden fazla kelime için de aynı şekilde çalışır.
 
+### Troll
+Bazı yazarları sadece bir entrysinden yola çıkarak bile, çok uzatmadan trollere eklemek:
+![hızlı trolllere gönderim](http://i.imgur.com/shYlt7X.png)
 
 # Kurulum
-Biraz daha geliştirdikten sonra Chrome Web Store'a göndereceğim. Web Store'a gönderene kadar güncellemeleri sizin takip etmeniz, kurmanız gerekecek. 
+Biraz daha olgunlaşınca Chrome Web Store'a göndereceğim. Web Store'a gönderene kadar güncellemeleri sizin takip etmeniz, kurmanız gerekecek.
 
 `chrome://extensions` > Geliştirici modu > Paketlenmemiş uzantı yükle > [*Uzantının klasörünü gösterin*]
 
 Güncellemeleri kolayca takip etmek için `git clone` yaptıktan sonra, zaman zaman güncellemeler için `git fetch` sonrası `chrome://extensions`'dan Ekşi Reaktör'ü `yenile`meniz lazım.
-
-
 
 ## Krediler
 - [Ekşi Sözlük Sol Frame Kaldırgacı](https://chrome.google.com/webstore/detail/ek%C5%9Fi-s%C3%B6zl%C3%BCk-sol-frame-kal/kahiifbhglbbanbmkkgklfccicbglcjl)
 - [EksiTube](https://github.com/metude/eksitube)
 - [EkşiSözlük Troll Butonu](https://chrome.google.com/webstore/detail/ek%C5%9Fi-s%C3%B6zl%C3%BCk-troll-butonu/lhgmmdnlaoppjfbjgdhokcipogmgcfcc)
 - [Logo](http://www.iconarchive.com/show/windows-8-icons-by-icons8/Industry-Nuclear-Power-Plant-icon.html)
-
--- eksin.user.js
+++ eksin.user.js
@@ -75,12 +75,10 @@ chrome.storage.sync.get({
   youtube: false
 }, function(items) {
 
-
   if (items.youtube) {
     var search_input = $("#title").text();
-    var keyword = encodeURIComponent(search_input);
-    // Youtube API
-    var yt_url =  "https://www.googleapis.com/youtube/v3/search?safeSearch=none&part=snippet&q="+ keyword +"&relevanceLanguage=tr&maxResults=1&key=AIzaSyBrQhJDh900EamsDLWZH6kQm_9Dc1AqcX8"
+
+    var yt_url =  "https://www.googleapis.com/youtube/v3/search?safeSearch=none&part=snippet&q="+ encodeURIComponent(search_input) +"&relevanceLanguage=tr&maxResults=1&key=AIzaSyBrQhJDh900EamsDLWZH6kQm_9Dc1AqcX8"
 
     $.ajax({
         type: "GET",
@@ -90,9 +88,8 @@ chrome.storage.sync.get({
           if (response.items) {
                 $.each(response.items, function (i, data) {
                     var video_id = data.id.videoId;
-                    //var video_frame = "<iframe src='https://www.youtube.com/embed/" + video_id + "' width='100%' height='350'  frameborder='0' type='text/html'></iframe>";
                     var video_frame = "<iframe width='100%' height='360' src='https://www.youtube.com/embed/"+ video_id + "?feature=player_embedded' frameborder='0' allowfullscreen></iframe>";
-                    if ($("#videos")) $("#videos").html(video_frame); // Result
+                    if ($("#videos")) $("#videos").html(video_frame);
                 }); //.each
             } //if.response
         } //.sucess
@@ -102,45 +99,61 @@ chrome.storage.sync.get({
 
 
 /*
-Yazar bilgisini altına troll butonu.
-
-$(".info")
-    .each(function() {
-        var user = $(this).find('.entry-author');
-        var parentFooter = $(this).parents('footer');
-        var feedBack = parentFooter.find('.feedback');
-        var responseArea = $('<span/>');
-        responseArea.addClass('response-area');
-        var userPage = $(user).attr('href');
-        var trollButton = $('<a/>');
-        trollButton.attr('title', 'troll');
-        trollButton.attr('href', '');
-        trollButton.html('troll');
-        trollButton.attr('style', 'font-weight:bold;');
-        var li = $('<li/>');
-        li.append(trollButton);
-        $(this).find('.dropdown-menu').append(li);
-
-        $.get(userPage, function(resp) {
-            var html = $.parseHTML(resp);
-            var link = $(html).find('#blocked-link').attr('data-url');
-            trollButton.attr('data-url', link);
-            trollButton.on('click', function() {
-                var $this = $(this);
-                if ($this.attr('data-url') === '#') {
-                    $this.remove();
-                    return false;
-                }
-
-                $.post($(this).attr('data-url'));
-                $this.remove();
-                responseArea.html('<strong style="color:red;">Kullanıcı engellendi.</strong>');
-                feedBack.append(responseArea);
-                window.setTimeout(function() {
-                    $(responseArea).remove();
-                }, 3000);
-                return false;
-            }); //trollButton.on('click')
-        }); //.get
-    });
+Yazarın yanındaki diğerin altına troll butonu.
 */
+
+chrome.storage.sync.get({
+  troll: true
+}, function(items) {
+
+  // deaktifse süreci durdur
+  if (!items.troll) return false;
+
+  //
+  $(".info")
+      .each(function() {
+          var user = $(this).find('.entry-author');
+          var parentFooter = $(this).parents('footer');
+          var feedBack = parentFooter.find('.feedback');
+          var responseArea = $('<span/>');
+          responseArea.addClass('response-area');
+          var userPage = $(user).attr('href');
+          var trollButton = $('<a/>');
+          trollButton.attr('title', 'troll');
+          trollButton.attr('href', '');
+          trollButton.html('troll');
+          trollButton.attr('style', 'font-weight:bold;');
+          var li = $('<li/>');
+          li.append(trollButton);
+          $(this).find('.dropdown-menu').append(li);
+
+              trollButton.on('click', function(e) {
+                e.preventDefault();
+
+                var $this = $(this);
+
+                $.get(userPage, function(resp) {
+                    var html = $.parseHTML(resp);
+                    var link = $(html).find('#blocked-link').attr('data-url');
+
+                    if ($this.attr('data-url') === '#') {
+                        $this.remove();
+                        return false;
+                    }
+
+                    $.post(link);
+                    responseArea.html('<strong style="color:red;">Kullanıcı engellendi.</strong>');
+                    feedBack.append(responseArea);
+                    window.setTimeout(function() {
+                        $(responseArea).remove();
+                    }, 3000);
+
+                  return false;
+                }); //.get*/
+
+                $this.remove();
+
+              }); //trollButton.on('click')
+      }); //$(".info")
+
+});
